974a31c64f016c7fac90b678ba6cbc008b2393a5
holybobrius <ilya.povaroff@gmail.com>
2020-11-24 14:28:53 +0300

    Refactored to use Classes

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let myLibrary = [];
 
-function Book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(author, title, pages, read) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
+    }
 }
+
 
 function addBook() {
     const newAuthor = document.querySelector("#author-input").value;
